a94b69688a9b86425d1e7e1660667745a8d1a951
FhriiPhri <fahripadang050908@gmail.com>
2025-12-16 14:30:03 +0700

    style: add button route to login at header landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -208,12 +208,16 @@ const LandingPage = () => {
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-20">
-            <div className="flex items-center space-x-3 group cursor-pointer">
+            <div
+              onClick={() => navigate("/login")}
+              className="flex items-center space-x-3 group cursor-pointer"
+            >
               <div className="relative">
                 <div className="w-10 h-10 rounded-xl flex items-center justify-center transform group-hover:scale-105 transition-all duration-300">
-                  <img className="" src={icon2} alt="Synapse Logo" />
+                  <img src={icon2} alt="Synapse Logo" />
                 </div>
               </div>
+
               <span
                 className={`text-2xl font-bold bg-gradient-to-r ${gradients.primary} bg-clip-text text-transparent`}
               >
